a09c194f30ef5f311234d08ec605b91ee0807ee6
Rowan Connaughton <rowanconnaughton@gmail.com>
2019-03-15 18:16:24 +0000

    updated databse

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '',
-        user: '',
-        password: '',
-        database: ''
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
